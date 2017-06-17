f767e6a890968c5a2e55b67a8fde5939d416b2b7
Mersace <ma815305712@outlook.com>
2017-06-17 16:25:38 +0800

    update

---
+++ MSCategories/MSCategories/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MSCategories
+//
+//  Created by Ashscar on 17/6/17.
+//  Copyright © 2017年 Ashscar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MSCategories/MSCategories/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MSCategories
+//
+//  Created by Ashscar on 17/6/17.
+//  Copyright © 2017年 Ashscar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
